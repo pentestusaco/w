74037d2fbb9c40051c95506db2685afaadb9449b
pentestusaco <usacoguidesilver@gmail.com>
2025-04-08 15:38:51 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
-sudo apt install python3 && sudo apt install python && yum install python3 && yum install python && pkg install python3 && pkg install python
-pip install cloudscraper requests && pip3 install cloudscraper requests
+#!/bin/bash
+sudo apt update -y
+sudo yum update -y
+sudo pkg update -y
+sudo apt install -y python3 python
+sudo yum install -y python3 python
+sudo pkg install -y python3 python
 cd /etc
-mkdir bot
+mkdir -p bot
 cd /etc/bot
-curl https://raw.githubusercontent.com/pentestusaco/w/main/bot.py
+wget https://raw.githubusercontent.com/pentestusaco/w/main/bot.py -O bot.py
 sudo python3 bot.py
